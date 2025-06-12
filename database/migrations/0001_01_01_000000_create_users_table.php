fedd7eeaa909700a035bc46ed0bee2dba6a32024
joseph-beginner-programmer <joseptan1512@outlook.com>
2025-06-12 11:06:37 +0700

    fix login

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('use', function (Blueprint $table) {
-            $table->id();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id("user_id");
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -6,11 +6,11 @@
 
         <div
             class="mt-5 shadow-[0_0_20px_rgba(0,0,0,0.15)] rounded-xl w-[90%] mx-auto px-2 py-3 flex flex-col items-center justify-center min-h-[10rem]">
-            <h1 class="text-blue-800 roboto font-bold text-2xl mb-3">
+            <h1 class="text-blue-800 monteserrat-title text-2xl mb-3">
                 Reservasi Venue Online Terbaik
             </h1>
-            <a href="/create" class="bg-blue-800 px-4 py-2 rounded-lg text-white hover:bg-blue-600 transition duration-100 ease-in-out">
-                Daftarkan Venue ->></a>
+            <a href="/create" class="bg-blue-800 px-4 py-2 monteserrat-body rounded-lg text-white hover:bg-blue-600 transition duration-100 ease-in-out">
+                Daftarkan Venue -></a>
         </div>
 
         <main class="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
