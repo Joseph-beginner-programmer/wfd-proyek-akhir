047a3845ca2f5e89524b07e56fb7c3c42268ff12
joseph-beginner-programmer <joseptan1512@outlook.com>
2025-06-12 16:15:02 +0700

    Booking Controller Added

--- app/Models/JadwalVenue.php
+++ app/Models/JadwalVenue.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class JadwalVenue extends Model
 {
-    //
+    public function bookings()
+    {
+        return $this->belongsToMany(JadwalVenue::class, 'booking_hour', 'booking_id', 'booking_hour_id')->withTimestamps();
+    }
 }
